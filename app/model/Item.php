c62236aa72d9e333d6bc907643786c50262c7252
João Mangueira <joao.mangueira@toxicologiapardini.com.br>
2021-04-08 10:42:27 -0300

    add estrutura

--- app/model/Item.php
+++ app/model/Item.php
@@ -3,8 +3,12 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['nicename', 'email', 'name', 'email', 'password', 'login'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
