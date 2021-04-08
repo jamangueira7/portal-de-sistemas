d8fa0dd2fa0d984c4cb74c8f01f40e3fb3edcfca
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-04-08 10:29:48 -0300

    add front

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+
+<script src="{{ asset('js/jquery-3-3-1.js') }}"></script>
+<script src="{{ asset('js/popper.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+</footer>
+</body>
+</html>
---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+  <div class="container-fluid gradient">
+    <div class="container text-right">
+      <ul class="mb-0 py-2 menu-sites">
+        <li class="menu_topo">
+          <li class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" id="menu-categorias" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Blog
+						</a>
+						<div class="dropdown-menu" aria-labelledby="menu-categorias">
+							<a href="#" class="dropdown-item">HTML</a>
+							<a href="#" class="dropdown-item">CSS</a>
+							<a href="#" class="dropdown-item">JS</a>
+						</div>
+					</li>
+        </li>
+        <li class="item_usuario">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">ACESSAR</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">NOME</a>
+          </li>
+        </li> 
+      </ul>
+    </div>  
+  </div>
+  
+    <div class="container-fluid bg_white mb-2">
+      <div class="row">
+        <div class="container">
+          <nav class="menu_topo_logo">
+            <div class="logo">
+              <a href="#">
+                <img src="/img/cropped-logo-160x60-1.png">
+              </a>
+            </div>
+          </nav>                       
+        </div>
+      </div>
+    </div>   
+  </header>
