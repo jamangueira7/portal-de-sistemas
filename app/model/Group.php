18cab74363e4c7a1b3f11caddd2136bf96248829
João Antônio Mangueira <jamjs7@gmail.com>
2021-04-08 11:46:25 -0300

    add estrutura (#3)
    Co-authored-by: João Mangueira <joao.mangueira@toxicologiapardini.com.br>

--- app/model/Group.php
+++ app/model/Group.php
@@ -3,8 +3,12 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['nicename', 'email', 'name', 'email', 'password', 'login'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
