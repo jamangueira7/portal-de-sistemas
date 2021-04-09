9b96082ac87a55751f46ef8c55715f6a39a9ce9a
João Antônio Mangueira <jamjs7@gmail.com>
2021-04-09 12:02:52 -0300

    Feature/back (#7)
    * add estrutura
    
    * Criando as primeiras rotas que são liberadas para todos os usuarios.
    
    Co-authored-by: João Mangueira <joao.mangueira@toxicologiapardini.com.br>

--- app/Http/Controllers/FreeController.php
+++ app/Http/Controllers/FreeController.php
@@ -8,6 +8,16 @@ class FreeController extends Controller
 {
     public function index()
     {
+        return view('home.index');
+    }
 
+    public function login()
+    {
+        return view('home.tela-login');
+    }
+
+    public function logout()
+    {
+        return view('home.tela-login');
     }
 }
