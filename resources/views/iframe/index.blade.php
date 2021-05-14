77640b00a45bd8d268565705fd016b28c584fa34
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-05-14 18:18:48 -0300

    add url no logo

--- resources/views/iframe/index.blade.php
+++ resources/views/iframe/index.blade.php
@@ -12,7 +12,8 @@
     <div class="row">
       <div class="col-2 logo">
         <a href="#">
-          <img src="/img/cropped-logo-160x60-1.png" class="pl-2">
+          <!-- <img src="/img/cropped-logo-160x60-1.png" class="pl-2"> -->
+          <img src="{{ asset('/img/cropped-logo-160x60-1.png') }}" class="pl-2">
         </a>
       </div>
 
