d47f17bad1e931c6b7e5b33b8f460ccb43c483cd
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-05-17 11:44:13 -0300

    atualizacao de prod

--- app/Http/Controllers/FreeController.php
+++ app/Http/Controllers/FreeController.php
@@ -65,7 +65,7 @@ class FreeController extends Controller
 
             session(['iPlanetDirectoryPro' => 'adafds']);
             session(['userName' => 'Elmer Mohr IV' ]);
-            session(['userID' => '2d243bf2-b28e-45dc-b686-6cb6e240500a' ]);
+            session(['userID' => 'a0bafc7e-b36d-11eb-8889-002170f7987a' ]);
             session(['userAccess' => true ]);
 
 
